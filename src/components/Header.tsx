32dbecf59ebdd1105543cc8eb6cc7eee80e2dc04
abdev <ab.inpathtoadev@gmail.com>
2023-11-19 17:14:28 +0530

    pagination added, major ui mods

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import SearchBox from '../components/SearchBox';
 
 const Header = () => {
     return (
-        <header className="bg-shark p-4 flex items-center justify-between mb-4">
+        <header className="p-4 flex items-center justify-between mb-4" style={{ background: "rgba(15,19,23,.8)" }}>
             <a href="/">
                 <h1 className="text-white text-2xl font-bold">CINE TRACK</h1>
             </a>
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,36 +2,77 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import TileGrid from '../components/TileGrid';
+import Pagination from '../components/Pagination';
+import SpinningLoader from '../components/SpinningLoader';
 
-import { SearchMedia, MediaSearchResult } from '../utils/omdbApi';
+import { SearchMedia, MediaSearchResult, MediaSearchResults } from '../utils/omdbApi';
+
+const ITEMS_PER_PAGE = 10; // Adjust this based on your requirements
 
 const SearchPage = () => {
-  const [searchParams,] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchResults, setSearchResults] = useState<MediaSearchResult[]>([]);
+  const [totalResults, setTotalResults] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const key = searchParams.get('key') || '';
-    setSearchQuery(key);
+    setSearchQuery(searchParams.get('key') || '');
+    setCurrentPage(searchParams.get('page') ? parseInt(searchParams.get('page') as string) : 1);
   }, [searchParams]);
 
   useEffect(() => {
     const fetchData = async () => {
-      let results: MediaSearchResult[] = [];
-      if (searchQuery && searchQuery.length >= 3)
-        results = await SearchMedia(searchQuery);
-      setSearchResults(results);
+      setLoading(true);
+      let results: MediaSearchResults = { results: [], totalResults: 0 };
+      if (searchQuery && searchQuery.length >= 3) {
+        results = await SearchMedia(searchQuery, currentPage);
+        setSearchResults(results["results"]);
+        setTotalResults(results["totalResults"]);
+        setLoading(false);
+      }
+      else {
+        setSearchResults([]);
+        setTotalResults(0);
+        setLoading(false);
+      }
     };
 
     fetchData();
-  }, [searchQuery]);
+  }, [searchQuery, currentPage]);
+
+  const renderUi = () => {
+    if (searchQuery.length < 3)
+      return (
+        <p className='text-xl'>Please search for something in searchbox. Search query must be at least 3 characters long.</p>
+      );
+
+    if (loading) {
+      return (
+        <SpinningLoader />
+      );
+    }
+
+    if (searchResults.length === 0)
+      return (
+        <p className='text-xl'>No results found</p>
+      );
+
+    return (<>
+      <TileGrid searchResults={searchResults} />
+      <Pagination
+        totalPages={Math.ceil(totalResults / ITEMS_PER_PAGE)}
+        currentPage={currentPage}
+        baseLink={`/search?key=${searchQuery}`}
+      />
+    </>);
+
+  };
 
   return (
-    <div className='flex items-center justify-center'>
-      <TileGrid
-        searchResults={searchResults}
-      />
-      {/* Add your search results and components here */}
+    <div className='flex items-center justify-center flex-col'>
+      {renderUi()}
     </div>
   );
 };
